0d1bd6543fe347cf08033889cb255e8061781a86
Aryan Srivastava <143885879+Sri-Aryan@users.noreply.github.com>
2025-08-25 21:32:10 +0530

    Fix: App Database class

--- app/src/main/java/com/example/letteblack/db/AppDatabase.kt
+++ app/src/main/java/com/example/letteblack/db/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.example.letteblack.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [GroupMemberEntity::class, NoteEntity::class, GroupEntity::class], version = 1, exportSchema = false)
+@Database(entities = [GroupMemberEntity::class, NoteEntity::class, GroupEntity::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun groupMemberDao(): GroupMemberDao
     abstract fun noteDao(): NoteDao
